69282bd4d3945c56d1fbdd84614863b65069d2d9
Krisa Maharjan <krisamhrjan@gmail.com>
2025-07-27 22:00:52 +0545

    Added missinng lines in setting.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Chatbot',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Backend.urls'
@@ -64,6 +66,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOW_ALL_ORIGINS= True
 
 WSGI_APPLICATION = 'Backend.wsgi.application'
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Chatbot-Movie-Recommendation
 Chatbot designed for movie recommendation system
+TO create frontent react app : npx create-react-app yourappname
+To craete a virtual environment: python3 -m venv venv
+                                source venv/bin/activate
+To initialize django : 1:pip install django
+2
